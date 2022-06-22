5af0df78132ac88b91c5ecb09020b0216dfad984
Miroslav Palka <palkamiroslav.v@gmail.com>
2022-06-22 15:21:59 +0100

    make board an array

--- spec/tic_tac_toe_spec.rb
+++ spec/tic_tac_toe_spec.rb
@@ -1,9 +1,16 @@
 require './tic_tac_toe'
 
 describe TicTacToe do
-	
-	it 'makes a grid for new instance' do
+
+	context 'new instance' do
 		new_game = TicTacToe.new
-		expect(new_game.board).to eq true
+		
+		it 'makes a board' do
+			expect(new_game.board).to be_truthy
+		end
+
+		it 'makes board an array' do
+			expect(new_game.board.class).to eq Array
+		end
 	end
 end
--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -2,6 +2,14 @@
 class TicTacToe
 	attr_accessor :board
 	def initialize
-		@board = true
+		@board = []
 	end
+
+  # def display_tictactoe(@board)
+  #   puts "#{board[0]} | #{board[1]} | #{board[2]}"
+  #   puts "---------------"
+  #   puts 
+  #   puts "---------------"
+  #   puts
+  # end
 end
