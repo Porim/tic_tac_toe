911263ce9a713b6586dfeca51dc2583557723bcb
Miroslav Palka <palkamiroslav.v@gmail.com>
2022-07-05 10:04:35 +0100

    work in progress...

--- spec/tic_tac_toe_spec.rb
+++ spec/tic_tac_toe_spec.rb
@@ -1,28 +1,53 @@
+# frozen_string_literal: true
+
 require './tic_tac_toe'
 
 describe TicTacToe do
+  context 'new instance' do
+    new_game = TicTacToe.new
 
-	context 'new instance' do
-		new_game = TicTacToe.new
-		
-		it 'makes a board' do
-			expect(new_game.board).to be_truthy
-		end
+    it 'makes a board' do
+      expect(new_game.board).to be_truthy
+    end
 
-		it 'makes board an array' do
-			expect(new_game.board.class).to eq Array
-		end
+    it 'makes board an array' do
+      expect(new_game.board.class).to eq Array
+    end
 
-		it 'makes board with length 9' do
-			expect(new_game.board.length).to eq 9
-		end
-	end
+    it 'makes board with length 9' do
+      expect(new_game.board.length).to eq 9
+    end
 
-	context '#display_tictactoe' do
+    it 'makes board with numbers 1 to 9' do
+      expect(new_game.board).to include(1,2,9)
+    end
+  end
 
-		it 'prints to terminal' do
-			expect {TicTacToe.new.display_tictactoe}.to output.to_stdout
-		end
+  context '#place_mark_on_board' do 
+    it 'places a X at given position' do 
+      new_game = TicTacToe.new
+      new_game.place_mark_on_board(1)
+      expect(new_game.board[0]).to eq 'X'
+    end
+    it 'places a O at given position' do 
+      new_game = TicTacToe.new
+      new_game.place_mark_on_board(1)
+      new_game.place_mark_on_board(2)
+      expect(new_game.board[1]).to eq 'O'
+    end
+  end
 
-	end
+  context '#anybody_won?' do
+    it '' do
+      new_game = TicTacToe.new
+      new_game.board = [0, 1 , 2, 'X', 'X', 'X', 6, 7, 8]
+      expect(new_game.anybody_won?).to eq true
+    end
+  end
+
+  context '#display_tictactoe' do
+    it 'prints to terminal' do
+      expect { TicTacToe.new.display_tictactoe }.to output.to_stdout
+    end
+  end
 end
--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,20 +1,87 @@
+# frozen_string_literal: true
+
+require 'sinatra'
+enable :sessions
+
 
 class TicTacToe
-	attr_accessor :board
-	def initialize
-		@board = Array.new(9,' ')
-	end
+  attr_accessor :board
+
+  def initialize
+    @board = (1..9).to_a
+    @player = 1
+  end
 
   def display_tictactoe
     puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
-    puts "-----------"
+    puts '-----------'
     puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
-    puts "-----------"
-   	puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+    puts '-----------'
+    puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+  end
+
+  # place mark
+  # check if current player wins
+  # if yes do something
+  # if no carry on and switch players
+  
+  def place_mark_on_board(user_choice)
+    if @player == 1
+      mark = 'X'
+      @player = 2
+    else
+      mark = 'O'
+      @player = 1
+    end
+    @board[user_choice - 1] = mark
+  end
+
+  def check_if_X_won
+    marks = []
+    @board.map.with_index do |mark, index|
+      marks << index if mark == "X"
+    end
+  end
+
+  def anybody_won?
+    win_combinations.each {|combo|
+      check = combo.map {|x| @board[x]}
+      return true if check.uniq.size == 1
+    }
+    
+    # win_combinations.each {|win_combination| marks.include?(win_combinations)}
+  end
+
+  def win_combinations
+    [ 
+    [0,1,2], # top_row 
+    [3,4,5], # middle_row 
+    [6,7,8], # bottom_row 
+    [0,3,6], # left_column 
+    [1,4,7], # center_column 
+    [2,5,8], # right_column 
+    [0,4,8], # left_diagonal 
+    [6,4,2] # right_diagonal 
+    ]
   end
 end
 
-TicTacToe.new.display_tictactoe
+get '/' do
+  session[:board] = {}
+  erb :game, locals: {:board => session[:board]}
+end
 
-print "Choose where you want to place your mark with numbers 1-9: "
-user = gets.chomp
+post '/' do 
+  
+
+
+
+# def game
+#   TicTacToe.new.display_tictactoe
+
+#   print 'Choose where you want to place your mark with numbers 1-9: '
+#   user_choice = gets.chomp
+#   user_choice.to_i
+# end
+
+
